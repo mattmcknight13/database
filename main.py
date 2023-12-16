8ef8ee8efa8479a701c6dbe248d39e13e01044fb
Matthew McKnight <matthewmcknight@Matthews-MacBook-Pro.local>
2023-12-15 20:25:58 -0600

    update todo list

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     cli_interface = CLIInterface(main_db_location, test_db_location)
     cli_interface.run()
 
-
+### TODO
 # make sure we can clean up (nuke) after tests
 # setup .toml file
+# fix create_table (think we need to instanciate different instances based on db)
+# add ability to create multiple tables at once to speed up start steps
+###
     
